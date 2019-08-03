55e29b5fb2bcf87ccef01976c847ac4879db932b
蔺绍宏 <shaohonglin0411@gmail.com>
2019-08-03 19:25:19 +0800

    add image site

--- image-site/src/store/index.ts
+++ image-site/src/store/index.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { IAppState } from './modules/app'
-import { ITagsViewState } from './modules/tags-view'
 import { IErrorLogState } from './modules/error-log'
 import { ISettingsState } from './modules/settings'
 
@@ -9,7 +8,6 @@ Vue.use(Vuex)
 
 export interface IRootState {
   app: IAppState
-  tagsView: ITagsViewState
   errorLog: IErrorLogState
   settings: ISettingsState
 }
--- image-site/src/utils/permission.ts
+++
@@ -1,3 +0,0 @@
-export const checkPermission = (value: string[]): boolean => {
-  return true
-}
