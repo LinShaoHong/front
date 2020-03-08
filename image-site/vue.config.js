e99d2721991364930309327e2cbd25cda55f0c58
LinShaoHong <lins@LinShaoHong-MacBook.local>
2020-03-08 20:23:34 +0800

    fix

--- image-site/vue.config.js
+++ image-site/vue.config.js
@@ -4,7 +4,7 @@ const mockServerPort = 1378
 const name = 'Image Site'
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/meizi/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
