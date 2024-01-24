c2b0af561c61dbc1df68130f0fa7e39a311e9b71
LinShaoHong <shaohonglin0411@gmail.com>
2024-01-24 15:55:36 +0800

    fix

--- uni-card/pages/common/card-mixins.js
+++ uni-card/pages/common/card-mixins.js
@@ -1,41 +1,39 @@
-import utils from '@/common/utils.js'
-
 export default {
-	data() {
-		return {
-			curIdx: 0,
-			array: []
-		}
-	},
-    methods: {
-		initCard(cardNum) {
-			if (cardNum < 1 || (this.array.length && this.array.length == cardNum)) return
-			this.array = []
-			for (let i = 1; i <= cardNum; i++) {
-				this.array.push(i)
-			}
-			this.shuffle()
-		},
-		drawCard() {
-			if (!this.array.length) return
-			if (this.curIdx > this.array.length - 1) {
-				this.shuffle()
-				return this.drawCard()
-			} else {
-				let val = this.array[this.curIdx]
-				this.curIdx++
-				return val
-			}
-		},
-		shuffle() {
-			this.curIdx = 0
-			let array = [...this.array]
-			for (let i = array.length - 1; i > 0; i--) {
-			    const j = Math.floor(Math.random() * (i + 1)); // 生成[0, i]之间的随机索引
-			    [array[i], array[j]] = [array[j], array[i]]; // 交换当前元素与随机选取的元素位置
-			}
-			this.array = array
-			console.log('shuffle', this.array);
-		}
+  data() {
+    return {
+      curIdx: 0,
+      array: []
     }
+  },
+  methods: {
+    initCard(cardNum) {
+      if (cardNum < 1 || (this.array.length && this.array.length === cardNum)) return
+      this.array = []
+      for (let i = 1; i <= cardNum; i++) {
+        this.array.push(i)
+      }
+      this.shuffle()
+    },
+    drawCard() {
+      if (!this.array.length) return
+      if (this.curIdx > this.array.length - 1) {
+        this.shuffle()
+        return this.drawCard()
+      } else {
+        let val = this.array[this.curIdx]
+        this.curIdx++
+        return val
+      }
+    },
+    shuffle() {
+      this.curIdx = 0
+      let array = [...this.array]
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); // 生成[0, i]之间的随机索引
+        [array[i], array[j]] = [array[j], array[i]]; // 交换当前元素与随机选取的元素位置
+      }
+      this.array = array
+      console.log('shuffle', this.array);
+    }
+  }
 }
