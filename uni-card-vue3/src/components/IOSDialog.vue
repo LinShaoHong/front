f0030b9a01f3da2ee31db812fdd85dcc30737ecc
LinShaoHong <shaohonglin0411@gmail.com>
2024-02-22 11:28:49 +0800

    fix

--- uni-card-vue3/src/components/IOSDialog.vue
+++ uni-card-vue3/src/components/IOSDialog.vue
@@ -6,20 +6,20 @@ const props = defineProps({
   show: Boolean
 });
 
-const share = () => {
-  console.log('share');
-  wxj.ready(function () {
-    wxj.updateAppMessageShareData({
-      title: 'aaa',
-      desc: 'bbb',
-      link: 'ccc',
-      imgUrl: 'ddd',
-      success: function () {
-        // 设置成功
-      }
-    })
-  });
-};
+// const share = () => {
+//   console.log('share');
+  // wxj.ready(function () {
+  //   wxj.updateAppMessageShareData({
+  //     title: 'aaa',
+  //     desc: 'bbb',
+  //     link: 'ccc',
+  //     imgUrl: 'ddd',
+  //     success: function () {
+  //       // 设置成功
+  //     }
+  //   })
+  // });
+// };
 
 </script>
 <template>
@@ -27,7 +27,7 @@ const share = () => {
     <view class="relative w-80vw flex flex-col items-center justify-center bg-white rd-10 gap-20 p-20">
       <text class="font-bold" style="font-size: 36rpx">温馨提示</text>
       <view class="break-all w-full p-20" v-html="config.data.value.iosText"></view>
-      <button open-type="share" style="background: transparent" @click="share">
+      <button open-type="share" style="background: transparent">
         <view class="h-65 w-250 mt-10 rd-40 text-white flex items-center justify-center"
               style="background: #482380; font-size: 32rpx; letter-spacing: 3rpx;">立即邀请
         </view>
