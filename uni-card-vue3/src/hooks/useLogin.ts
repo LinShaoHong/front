001f84fbeafc9da6a6c9d548422862764cc4c67e
LinShaoHong <shaohonglin0411@gmail.com>
2024-02-22 10:47:14 +0800

    fix

--- uni-card-vue3/src/hooks/useLogin.ts
+++ uni-card-vue3/src/hooks/useLogin.ts
@@ -40,7 +40,7 @@ export function useLogin() {
           const secret = '6f7e83f1be4c343254e6c2602394e7e5';
           const redictUrl = encodeURIComponent(window.location.href);
           window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?' +
-            'appid=' + appId + '&redirect_uri=' + redictUrl + '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+            'appid=' + appId + '&redirect_uri=' + redictUrl + '&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
         } else {
           uni.setStorageSync('code', code);
         }
