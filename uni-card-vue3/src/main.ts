69cf25c13a4ff47c2bb8e74bea68ebd2f715eb5a
LinShaoHong <shaohonglin0411@gmail.com>
2024-02-22 15:44:56 +0800

    fix

--- uni-card-vue3/src/main.ts
+++ uni-card-vue3/src/main.ts
@@ -2,12 +2,15 @@ import 'uno.css';
 import './style/index.css'
 import { createSSRApp } from 'vue';
 import App from './App.vue';
+import vant from 'vant';
+import 'vant/lib/index.css';
 import store from '@/store';
 import plugin from '@/plugin';
 
 export function createApp() {
   const app = createSSRApp(App);
   app.use(store);
+  app.use(vant);
   app.use(plugin);
   return {
     app
