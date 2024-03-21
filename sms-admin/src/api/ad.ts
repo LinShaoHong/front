1d3dfed428512a4da78c23e3a66c16061ac386e6
LinShaoHong <shaohonglin0411@gmail.com>
2024-03-21 12:58:30 +0800

    fix

--- sms-admin/src/api/ad.ts
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-import { SingleResp } from './response'
-
-export const genSentence = (): SingleResp | any =>
-  request({
-    url: '/v1/qm/admin/ad/gen/sentence',
-    method: 'get'
-  })
--- sms/src/constant/image.ts
+++
@@ -1,9 +0,0 @@
-export abstract class V_IMAGE {
-  static readonly width = 170
-  static readonly height = 250
-
-  static readonly mobile = {
-    width: 164,
-    height: 230
-  }
-}
